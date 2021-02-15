5373392aacb996875d0ec71329c1889e806a3a72
tomoaki-abe <zhizhangabu32@gmail.com>
2021-02-15 16:33:46 +0900

    Revert "gemfile修正"
    This reverts commit 1deb1034373be932efc43d03356cdd9c4cd3935b.

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,3 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'posts#index'
-  get 'webhook/kokodayo'
-  post '/callback' => 'linebot#callback'
-
-  resources :posts, only: [:index] do
-    collection do
-      get 'project'
-      get 'start'
-      get 'mypage'
-      get 'detailed'
-    end
-  end
-  
+  root 'items#index'
 end
