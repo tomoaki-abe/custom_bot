bb6836b915f88f40c2628c91e4cda8b6087c99ca
tomoaki-abe <zhizhangabu32@gmail.com>
2021-03-07 21:55:32 +0900

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  root 'items#index'
+  devise_for :users
+  root to: 'posts#index'
+  get 'webhook/kokodayo'
+  post '/callback' => 'linebot#callback'
+
+  resources :flonts, only: [:index] do
+    collection do
+      get 'project'
+      get 'start'
+      get 'mypage'
+      get 'detailed'
+    end
+  end
+  
 end
